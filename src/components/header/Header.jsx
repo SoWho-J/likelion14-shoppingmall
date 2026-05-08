45b464faa108f22c24c46a68289d77daf2618013
sowho.j <jangseohu112@gmail.com>
2026-05-08 17:58:24 +0900

    헤더 버튼 이벤트 연결 및 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,58 +1,98 @@
 import styled from "styled-components";
 import logoUrl from "../../assets/images/kream_image.png";
-import homeUrl from "../../assets/icons/home_icon.png";
 import { useLocation, useNavigate } from "react-router-dom";
 
-// 대문자로 시작! -> 대문자를 컨포넌트로 인식하기 때문
 const LogoImage = styled.img`
   width: 166px;
   height: 141px;
-`;
-
-const HomeIcon = styled.img`
-  width: 61px;
-  height: 24px;
+  cursor: pointer;
 `;
 
 const HeaderContainer = styled.div`
-  padding-right: 160px;
-  padding-left: 160px;
+  padding: 0 160px;
+  height: 110px;
+
   display: flex;
   justify-content: space-between;
-`;
+  align-items: center;
 
-const Button = styled.div`
-  color: #6c6c6c;
-  font-size: 13px;
-  font-family: Pretendard;
-  font-weight: 400;
-  margin-top: 9px;
+  box-sizing: border-box;
 `;
 
 const HeaderRight = styled.div`
+  display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  display: inline-flex;
   align-items: flex-end;
+  gap: 28px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
   gap: 36px;
 `;
 
+const HeaderButton = styled.button`
+  border: none;
+  background: transparent;
+
+  color: ${({ $active }) => ($active ? "#000" : "#6c6c6c")};
+
+  font-family: "Pretendard", sans-serif;
+  font-size: 13px;
+  font-weight: 400;
+
+  cursor: pointer;
+
+  &:hover {
+    color: #000;
+  }
+`;
+
+const HomeButton = styled.button`
+  border: none;
+  background: transparent;
+
+  color: #111;
+  font-family: "Pretendard", sans-serif;
+  font-size: 24px;
+  font-weight: 400;
+
+  cursor: pointer;
+`;
+
 export default function Header() {
-  const { pathname } = useLocation(); // 현재 페이지 경로 불러오기
+  const { pathname } = useLocation();
   const navigate = useNavigate();
-  const buttonName = "상품등록";
+
+  const isMainPage = pathname === "/";
+  const isAddPage = pathname === "/add";
+  const isDetailPage = pathname.startsWith("/item/");
 
   return (
-    <div>
-      <HeaderContainer>
-        <LogoImage src={logoUrl} />
-        <HeaderRight>
-          {pathname === "/" && (
-            <Button onClick={() => navigate("/add")}>{buttonName}</Button>
+    <HeaderContainer>
+      <LogoImage src={logoUrl} onClick={() => navigate("/")} />
+
+      <HeaderRight>
+        <ButtonRow>
+          {(isMainPage || isAddPage || isDetailPage) && (
+            <HeaderButton $active={isAddPage} onClick={() => navigate("/add")}>
+              상품등록
+            </HeaderButton>
           )}
-          <HomeIcon src={homeUrl} />
-        </HeaderRight>
-      </HeaderContainer>
-    </div>
+
+          {isDetailPage && (
+            <>
+              <HeaderButton onClick={() => navigate(`${pathname}?delete=true`)}>
+                상품삭제
+              </HeaderButton>
+
+              <HeaderButton>상품수정</HeaderButton>
+            </>
+          )}
+        </ButtonRow>
+
+        <HomeButton onClick={() => navigate("/")}>HOME</HomeButton>
+      </HeaderRight>
+    </HeaderContainer>
   );
 }
